0e100d93d47a23507845fe5e178517831aa4227a
Thomas Friebel <thomas.friebel@amd.com>
2012-10-05 09:28:38 +0200

    Fix typo in license name

--- create_omtb_garmin_img.sh
+++ create_omtb_garmin_img.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-# License: Creative Commons Share Alive 3.0
+# License: Creative Commons Share Alike 3.0
 # Copyright: 2012, Bernhard Tittelbach <xro@realraum.at>
 # Thanks to malenki on #osm-de@oftc.net for constructive input and nagging me into making this thing useable in the first place
 
